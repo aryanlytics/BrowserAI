b2737f1a1302be91540d22374cf648b6b975dbb5
Aryan <aryanlytics@gmail.com>
2026-06-11 12:30:14 +0500

    refactor: remove legacy API and auth-client files and add Zod validation schemas

--- apps/web-frontend/lib/api.tsx
+++
@@ -1,9 +0,0 @@
-// apps/web/lib/api.ts
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "http://localhost:4000", // always api-gateway
-  withCredentials: true,            // send cookies
-});
-
-export default api;
