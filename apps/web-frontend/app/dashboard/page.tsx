09aa3b29c204060b175f62dbfc50efffa71de717
Aryan <aryanlytics@gmail.com>
2026-06-27 23:04:28 +0500

    feat: implement Speechmatics temporary token endpoint and add frontend microphone trigger UI

--- apps/web-frontend/app/dashboard/page.tsx
+++ apps/web-frontend/app/dashboard/page.tsx
@@ -1,8 +1,36 @@
+"use client"
+
+import api from '@/lib/api'
 import React from 'react'
+import { toast } from 'sonner'
 
 const Dashboard = () => {
+  const handlemic = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    try {
+      const response = await api.post('/api/voice/temp-token')
+      console.log(response.data)
+    } catch (error) {
+      const axiosError = error as { response?: { data?: { message?: string } } };
+      const message = axiosError.response?.data?.message ?? "Failed to get temp token";
+      toast.error("Failed to get temp token", {
+        description: message,
+      });
+    }
+
+
+  }
   return (
-    <div className='w-screen h-screen'>Dashboard</div>
+    <div className='w-screen h-screen'>
+      <div>
+          Dasboaord
+      </div>
+      <div>
+          <button onClick={handlemic}>
+             Mic
+          </button>
+      </div>
+    </div>
   )
 }
 
