8d0b0ad3ef42c1e4b484e8d61a9757ee6dc2f5dc
Aryan <aryanlytics@gmail.com>
2026-06-27 23:04:54 +0500

    feat: add route to retrieve temporary Speechmatics tokens for voice service

--- apps/voice-service/src/routes/voice.routes.ts
+++ apps/voice-service/src/routes/voice.routes.ts
@@ -1,4 +1,44 @@
-// apps/voice-service/src/routes/voice.routes.ts
+import axios from "axios";
+import { config } from "../config/config.js";
+import type { FastifyPluginAsync } from "fastify";
 
-import { config } from '../config/config.js'
 
+const voiceRoutes: FastifyPluginAsync = async (fastify) => {
+    // ─── TEMP TOKEN ───────────────────────────────────────────────────────────────
+    fastify.post("/api/voice/temp-token", async (request, reply) => {
+        try{
+            const res = await axios.post(
+                `https://mp.speechmatics.com/v1/api_keys?type=rt`,
+                { ttl: 300 },
+                {
+                    headers: {
+                        Authorization: `Bearer ${config.SPEECHMATICS_API_KEY}`,
+                        'Content-Type': 'application/json',
+                    },
+                }
+            );
+            return reply.status(200).send({
+                token: res.data.key_value,
+                expiresIn: 300,
+            })
+        } catch (err) {
+            if (axios.isAxiosError(err)) {
+                const errText =
+                    typeof err.response?.data === 'string'
+                        ? err.response.data
+                        : JSON.stringify(err.response?.data ?? err.message)
+
+                request.log.error(`[Speechmatics] Failed to get token: ${errText}`)
+            } else {
+                request.log.error(err, 'Voice token error')
+            }
+
+            return reply.status(500).send({
+                statusCode: 500,
+                error: 'Internal Server Error',
+                message: 'Failed to request voice token',
+            })
+        }
+    })
+}
+export default voiceRoutes
