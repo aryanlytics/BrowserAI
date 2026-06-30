4e4fe31cfca3accdb92c7f2c934494068927e3d5
Aryan <aryanlytics@gmail.com>
2026-06-30 09:43:05 +0500

    refactor: export useSilero hook and return audio stream from media devices

--- apps/web-frontend/lib/hooks/silero-wrapper.ts
+++ apps/web-frontend/lib/hooks/silero-wrapper.ts
@@ -1,4 +1,4 @@
-const useSilero = async() => {
-    const stream = await navigator.mediaDevices.getUserMedia({ audio: true })
-    
+export const useSilero = async () => {
+  const stream = await navigator.mediaDevices.getUserMedia({ audio: true })
+  return stream
 }
