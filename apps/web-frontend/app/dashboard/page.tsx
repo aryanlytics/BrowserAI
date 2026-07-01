db0d2084aebac3235c3d4f2a3e3c1173e5fcba76
Aryan <aryanlytics@gmail.com>
2026-07-01 21:19:39 +0500

    feat: implement LiveKit voice token generation and update frontend mic integration

--- apps/web-frontend/app/dashboard/page.tsx
+++ apps/web-frontend/app/dashboard/page.tsx
@@ -5,20 +5,27 @@ import { useState } from "react"
 
 const Dashboard = () => {
   const [isListening, setIsListening] = useState(false)
+  const [token, setToken] = useState("")
   const handleMicClick = async () => {
     try {
       setIsListening(true)
       const response = await api.post("/api/voice/temp-token")
+      if (response.data.token) {
+        setToken(response.data.token)
+        
+      }
       console.log(response)
     } catch (error) {
       console.error(error)
     }
-    
+    finally {
+      setIsListening(false)
+    }
   }
   return (
     <div>
       <div>Dashboard</div>
-      <button onClick={handleMicClick}>Mic</button>
+      <button disabled={isListening} onClick={handleMicClick}>{isListening ? "Stop using your voice" : "control browser with your voice"}</button>
     </div>
   )
 }
